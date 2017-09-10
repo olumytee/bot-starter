3d84ebdd922efcb597486d8df7ad4e236c142600
olumide ojo <olumytee.ojo@gmail.com>
2017-09-10 18:03:35 +0100

    update readme and package.jspn

--- src/bot/Connector.ts
+++ src/bot/Connector.ts
@@ -6,7 +6,7 @@ const params = {
     appId: '',
     appPassword: ''
   },
-  prodcution: {
+  production: {
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
   }
